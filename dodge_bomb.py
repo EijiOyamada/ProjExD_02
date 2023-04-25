ce68634db9f92328424be260ded6c07ca421530c
eiji <c0b20040d2@edu.teu.ac.jp>
2023-04-25 14:26:50 +0900

    練習3

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -10,12 +10,14 @@ def main():
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
 
-    bb_img = pg.Surface((20, 20)) 
+    bb_img = pg.Surface((20, 20))
     pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)
     bb_img.set_colorkey((0, 0, 0))
 
     bb_rct = bb_img.get_rect()
     bb_rct.center = random.randint(0, 1600), random.randint(0, 900)
+
+    vx, vy = +1, +1
 
     tmr = 0
 
@@ -27,7 +29,9 @@ def main():
         tmr += 1
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, [900, 400])
-        screen.blit(bb_img, [0, 0])
+        
+        bb_rct.move_ip(vx, vy)
+        screen.blit(bb_img, bb_rct)
 
         pg.display.update()
         clock.tick(1000)
